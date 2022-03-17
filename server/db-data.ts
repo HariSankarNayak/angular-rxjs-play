68ef74f1cc017fa2ccc12108f2cc4a536afe9848
Hari Sankar Nayak <harisankar.nayak@nettantra.net>
2022-03-17 12:15:45 +0530

    initial commit

---
+++ server/db-data.ts
@@ -0,0 +1,351 @@
+
+
+export const COURSES: any = {
+
+    0: {
+        id: 0,
+        description: 'Description for Title',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        longDescription: 'Dumy Text for long description',
+        category: 'BEGINNER',
+        lessonsCount: 10
+    },
+
+    1: {
+        id: 1,
+        description: 'Description for Title',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        longDescription: 'Dumy Text for long description',
+        category: 'BEGINNER',
+        lessonsCount: 10
+    },
+    2: {
+        id: 2,
+        description: 'Description for Title',
+        longDescription: 'Dumy Text for long description',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        category: 'ADVANCED',
+        lessonsCount: 11
+    },
+    3: {
+        id: 3,
+        description: 'Description for Title',
+        longDescription: 'Dumy Text for long description',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        category: 'ADVANCED',
+        lessonsCount: 8
+    },
+    4: {
+        id: 4,
+        description: 'Description for Title',
+        longDescription: 'Dumy Text for long description',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        category: 'ADVANCED'
+    },
+    5: {
+        id: 5,
+        description: 'Description for Title',
+        longDescription: 'Dumy Text for long description',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        category: 'ADVANCED'
+    },
+    6: {
+        id: 6,
+        description: 'Description for Title',
+        longDescription: 'Dumy Text for long description',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        category: 'BEGINNER'
+    },
+    7: {
+        id: 7,
+        description: 'Description for Title',
+        longDescription: 'Dumy Text for long description',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        category: 'BEGINNER'
+    },
+    8: {
+        id:8,
+        description: 'Description for Title',
+        iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+        courseListIcon: 'https://camo.githubusercontent.com/bc109ebf393b45e7e54faaf8d9bc9c7866883640a5682be7c8529be55edf1149/68747470733a2f2f7332373338392e7063646e2e636f2f77702d636f6e74656e742f75706c6f6164732f323031392f30382f41646f626553746f636b5f3234343637353435322e6a706567',
+        longDescription: 'Dumy Text for long description',
+        category: 'ADVANCED'
+    },
+};
+
+
+export const LESSONS = {
+    1: {
+        id: 1,
+        "description": 'Description for Title',
+        "duration": "4:17",
+        "seqNo": 1,
+        courseId: 1
+    },
+    2: {
+        id: 2,
+        "description": 'Description for Title',
+        "duration": "2:07",
+        "seqNo": 2,
+        courseId: 1
+    },
+    3: {
+        id: 3,
+        "description": 'Description for Title',
+        "duration": "2:33",
+        "seqNo": 3,
+        courseId: 1
+    },
+    4: {
+        id: 4,
+        "description": 'Description for Title',
+        "duration": "4:44",
+        "seqNo": 4,
+        courseId: 1
+    },
+    5: {
+        id: 5,
+        "description": 'Description for Title',
+        "duration": "2:55",
+        "seqNo": 5,
+        courseId: 1
+    },
+    6: {
+        id: 6,
+        "description": 'Description for Title',
+        "duration": "3:27",
+        "seqNo": 6,
+        courseId: 1
+    },
+    7: {
+        id: 7,
+        "description": 'Description for Title',
+        "duration": "9:22",
+        "seqNo": 7,
+        courseId: 1
+    },
+    8: {
+        id: 8,
+        "description": 'Description for Title',
+        "duration": "1:26",
+        "seqNo": 8,
+        courseId: 1
+    },
+    9: {
+        id: 9,
+        "description": 'Description for Title',
+        "duration": "2:08",
+        "seqNo": 9,
+        courseId: 1
+    },
+    10: {
+        id: 10,
+        "description": 'Description for Title',
+        "duration": "4:01",
+        "seqNo": 10,
+        courseId: 1
+    },
+
+
+    // Security Course
+    11: {
+        id: 11,
+        "description": 'Description for Title',
+        "duration": "08:19",
+        "seqNo": 1,
+        courseId: 2
+    },
+
+    12: {
+        id: 12,
+        "description": 'Description for Title',
+        "duration": "04:17",
+        "seqNo": 2,
+        courseId: 2
+    },
+
+    13: {
+        id: 13,
+        "description": 'Description for Title',
+        "duration": "06:05",
+        "seqNo": 3,
+        courseId: 2
+    },
+
+    14: {
+        id: 14,
+        "description": 'Description for Title',
+        "duration": "03:57",
+        "seqNo": 4,
+        courseId: 2
+    },
+
+    15: {
+        id: 15,
+        "description": 'Description for Title',
+        "duration": "06:00",
+        "seqNo": 5,
+        courseId: 2
+    },
+    16: {
+        id: 16,
+        "description": 'Description for Title',
+        "duration": "04:53",
+        "seqNo": 6,
+        courseId: 2
+    },
+    17: {
+        id: 17,
+        "description": 'Description for Title',
+        "duration": "09:14",
+        "seqNo": 7,
+        courseId: 2
+    },
+    18: {
+        id: 18,
+        "description": 'Description for Title',
+        "duration": "06:08",
+        "seqNo": 8,
+        courseId: 2
+    },
+    19: {
+        id: 19,
+        "description": 'Description for Title',
+        "duration": "08:50",
+        "seqNo": 9,
+        courseId: 2
+    },
+    20: {
+        id: 20,
+        "description": 'Description for Title',
+        "duration": "05:46",
+        "seqNo": 10,
+        courseId: 2
+    },
+    21: {
+        id: 21,
+        "description": 'Description for Title',
+        "duration": "06:31",
+        "seqNo": 11,
+        courseId: 2
+    },
+
+
+    // PWA course
+
+    22: {
+        id: 22,
+        "description": 'Description for Title',
+        "duration": "07:19",
+        "seqNo": 1,
+        courseId: 3
+    },
+    23: {
+        id: 23,
+        "description": 'Description for Title',
+        "duration": "6:59",
+        "seqNo": 2,
+        courseId: 3
+    },
+    24: {
+        id: 24,
+        "description": 'Description for Title',
+        "duration": "7:28",
+        "seqNo": 3,
+        courseId: 3
+    },
+    25: {
+        id: 25,
+        "description": 'Description for Title',
+        "duration": "10:17",
+        "seqNo": 4,
+        courseId: 3
+    },
+
+    26: {
+        id: 26,
+        "description": 'Description for Title',
+        "duration": "09:50",
+        "seqNo": 5,
+        courseId: 3
+    },
+    27: {
+        id: 27,
+        "description": 'Description for Title',
+        "duration": "04:44",
+        "seqNo": 6,
+        courseId: 3
+    },
+    28: {
+        id: 28,
+        "description": 'Description for Title',
+        "duration": "06:07",
+        "seqNo": 7,
+        courseId: 3
+    },
+    29: {
+        id: 29,
+        "description": 'Description for Title',
+        "duration": "5:38",
+        "seqNo": 8,
+        courseId: 3
+    },
+
+    30: {
+        id: 30,
+        "description": 'Description for Title',
+        "duration": "4:17",
+        "seqNo": 1,
+        courseId: 0
+    },
+    31: {
+        id: 31,
+        "description": 'Description for Title',
+        "duration": "2:07",
+        "seqNo": 2,
+        courseId: 0
+    },
+    32: {
+        id: 32,
+        "description": 'Description for Title',
+        "duration": "2:33",
+        "seqNo": 3,
+        courseId: 0
+    },
+    34: {
+        id: 33,
+        "description": 'Description for Title',
+        "duration": "4:44",
+        "seqNo": 4,
+        courseId: 0
+    },
+    35: {
+        id: 35,
+        "description": 'Description for Title',
+        "duration": "2:55",
+        "seqNo": 5,
+        courseId: 0
+    },
+
+
+
+
+
+};
+
+export function findCourseById(courseId:number) {
+    return COURSES[courseId];
+}
+
+export function findLessonsForCourse(courseId:number) {
+    return Object.values(LESSONS).filter(lesson => lesson.courseId == courseId);
+}
+
